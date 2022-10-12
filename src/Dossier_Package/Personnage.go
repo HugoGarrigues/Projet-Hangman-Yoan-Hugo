ea8ad0383a898556b4384e09cd5f401caec8f76b
yoanvlt <113009422+yoanvlt@users.noreply.github.com>
2022-10-12 14:16:48 +0200

    coucou

---
+++ src/Dossier_Package/Personnage.go
@@ -0,0 +1,7 @@
+package Dossier_Package
+
+type Joueur struct{
+	nom string
+	lettre_choisi string
+	mot_choisi string
+}
---
+++ src/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "src/Dossier_Package"
+
+func main() {
+	Dossier_Package.Dossier()
+}
